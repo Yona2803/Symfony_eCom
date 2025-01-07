0bd8f2ce8c0a6a6a99c605a5d8c8c8e819e127a8
Youness Najeh <122638876+Yona2803@users.noreply.github.com>
2025-01-07 16:09:14 +0100

    Adding LandingPage, icon, img, style, js - 07/01/2025

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'homePage')]
+    public function index()
+    {
+        return $this->render('LandingPage/LandingPage.html.twig');
+    }
+}
