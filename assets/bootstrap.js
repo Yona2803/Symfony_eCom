e4c7602aed3552939fbd5335aa7fccf0db43f043
Youness Najeh <122638876+Yona2803@users.noreply.github.com>
2025-03-06 13:49:46 +0000

    Customizing HTTPs Errors - 06/03/2025

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,6 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+import { Application } from "@hotwired/stimulus";
+import { startStimulusApp } from "@symfony/stimulus-bundle";
 
-const app = startStimulusApp();
+const app = startStimulusApp(Application);
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,20 +2,14 @@
 
 namespace App\Controller;
 
-use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Items;
 use App\Service\CategoriesService;
 use App\Service\ItemsService;
 
 class HomePageController extends AbstractController
 {
-
     public function __construct(
         private ItemsService $itemsService,
         private CategoriesService $categoriesService
@@ -23,8 +17,6 @@ class HomePageController extends AbstractController
         $this->itemsService = $itemsService;
         $this->categoriesService = $categoriesService;
     }
-
-
 
     #[Route('/home', name: 'home')]
     public function getAll(): Response
@@ -36,7 +28,6 @@ class HomePageController extends AbstractController
             'categories' => $categories
         ]);
     }
-
 
     #[Route('/', name: 'homePage')]
     public function redirectToHomePage()
